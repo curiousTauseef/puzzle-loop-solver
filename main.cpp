3ef9b45ef90e648560fbb9275079a6ecc442715f
cyq <dz1933003@smail.nju.edu.cn>
2020-06-06 19:20:46 +0800

    ban around zero

--- main.cpp
+++ main.cpp
@@ -12,31 +12,39 @@ class puzzle
 public:
     size_t cols, rows = 0;   // of lattice
     vector<vector<int>> lat; // lattice
-    vector<vector<int>> hrz; // horizon link
-    vector<vector<int>> vrt; // vertical link
 
-    vector<vector<bool>> ban; // banned point
+    enum edge_state
+    {
+        BAN = -1,   // banned
+        NOT = 0,    // not linked
+        LINKED = 1, // linked
+        MUST = 2    // must linked
+    };
+    vector<vector<edge_state>> hrz; // horizon link
+    vector<vector<edge_state>> vrt; // vertical link
+
+    vector<vector<bool>> banned_point;
 
     size_t get_conn(const int &p_r, const int &p_c)
     {
         size_t conn = 0;
-        if (point_has_up(p_r, p_c))
-            conn += vrt[p_r - 1][p_c];
-        if (point_has_down(p_r, p_c))
-            conn += vrt[p_r][p_c];
-        if (point_has_left(p_r, p_c))
-            conn += hrz[p_r][p_c - 1];
-        if (point_has_right(p_r, p_c))
-            conn += hrz[p_r][p_c];
+        if (point_can_up(p_r, p_c) && vrt_has_edge(p_r - 1, p_c))
+            conn++;
+        if (point_can_down(p_r, p_c) && vrt_has_edge(p_r, p_c))
+            conn++;
+        if (point_can_left(p_r, p_c) && hrz_has_edge(p_r, p_c - 1))
+            conn++;
+        if (point_can_right(p_r, p_c) && hrz_has_edge(p_r, p_c))
+            conn++;
         return conn;
     }
 
     size_t lat_edge(const int &lat_r, const int &lat_c)
     {
-        return hrz[lat_r][lat_c] +
-               hrz[lat_r + 1][lat_c] +
-               vrt[lat_r][lat_c] +
-               vrt[lat_r][lat_c + 1];
+        return hrz_has_edge(lat_r, lat_c) +
+               hrz_has_edge(lat_r + 1, lat_c) +
+               vrt_has_edge(lat_r, lat_c) +
+               vrt_has_edge(lat_r, lat_c + 1);
     }
 
     bool hrz_sat(const int &h_r, const int &h_c)
@@ -65,40 +73,58 @@ public:
         return true;
     }
 
-    bool point_has_up(const int &p_r, const int &p_c)
+    bool point_can_up(const int &p_r, const int &p_c)
     {
         if (p_r <= 0)
             return false;
-        if (ban[p_r - 1][p_c])
+        if (banned_point[p_r - 1][p_c])
+            return false;
+        if (vrt[p_r - 1][p_c] == puzzle::BAN)
             return false;
         return true;
     }
 
-    bool point_has_down(const int &p_r, const int &p_c)
+    bool point_can_down(const int &p_r, const int &p_c)
     {
         if (p_r >= rows)
             return false;
-        if (ban[p_r + 1][p_c])
+        if (banned_point[p_r + 1][p_c])
+            return false;
+        if (vrt[p_r][p_c] == puzzle::BAN)
             return false;
         return true;
     }
 
-    bool point_has_left(const int &p_r, const int &p_c)
+    bool point_can_left(const int &p_r, const int &p_c)
     {
         if (p_c <= 0)
             return false;
-        if (ban[p_r][p_c - 1])
+        if (banned_point[p_r][p_c - 1])
+            return false;
+        if (hrz[p_r][p_c - 1] == puzzle::BAN)
             return false;
         return true;
     }
 
-    bool point_has_right(const int &p_r, const int &p_c)
+    bool point_can_right(const int &p_r, const int &p_c)
     {
         if (p_c >= cols)
             return false;
-        if (ban[p_r][p_c + 1])
+        if (banned_point[p_r][p_c + 1])
+            return false;
+        if (hrz[p_r][p_c] == puzzle::BAN)
             return false;
         return true;
+    }
+
+    int hrz_has_edge(const int &h_r, const int &h_c)
+    {
+        return hrz[h_r][h_c] == LINKED || hrz[h_r][h_c] == MUST;
+    }
+
+    int vrt_has_edge(const int &v_r, const int &v_c)
+    {
+        return vrt[v_r][v_c] == LINKED || vrt[v_r][v_c] == MUST;
     }
 
     bool hrz_has_up_lat(const int &h_r, const int &h_c)
@@ -150,7 +176,7 @@ public:
                     ss << dot;
                 if (col < cols)
                 {
-                    if (hrz[row][col])
+                    if (hrz_has_edge(row, col))
                         ss << link;
                     else
                         ss << " ";
@@ -161,7 +187,7 @@ public:
             {
                 for (size_t col = 0; col <= cols; col++)
                 {
-                    if (vrt[row][col])
+                    if (vrt_has_edge(row, col))
                         ss << link;
                     else
                         ss << " ";
@@ -180,7 +206,9 @@ public:
     }
 };
 
-void solve(puzzle &p);
+void heuristic(puzzle &p);
+
+void DFS(puzzle &p);
 void go_vrt(puzzle p,
             const int &start_r, const int &start_c,
             const int &v_r, const int &v_c,
@@ -234,19 +262,19 @@ int main(int argc, char **argv)
     // init connect state table
     for (size_t row = 0; row < p.rows + 1; row++)
     {
-        vector<int> tmp_row;
+        vector<puzzle::edge_state> tmp_row;
         for (size_t col = 0; col < p.cols; col++)
         {
-            tmp_row.push_back(0);
+            tmp_row.push_back(puzzle::NOT);
         }
         p.hrz.push_back(tmp_row);
     }
     for (size_t row = 0; row < p.rows; row++)
     {
-        vector<int> tmp_row;
+        vector<puzzle::edge_state> tmp_row;
         for (size_t col = 0; col < p.cols + 1; col++)
         {
-            tmp_row.push_back(0);
+            tmp_row.push_back(puzzle::NOT);
         }
         p.vrt.push_back(tmp_row);
     }
@@ -258,26 +286,47 @@ int main(int argc, char **argv)
         {
             tmp_row.push_back(false);
         }
-        p.ban.push_back(tmp_row);
+        p.banned_point.push_back(tmp_row);
     }
     // solve puzzle
-    solve(p);
+    heuristic(p);
+    DFS(p);
     cout << "Solutions: " << puzzle_results.size() << endl;
     return 0;
 }
 
-void solve(puzzle &p)
+void heuristic(puzzle &p)
 {
+    // ban around zero
+    for (size_t row = 0; row < p.rows; row++)
+    {
+        for (size_t col = 0; col < p.cols; col++)
+        {
+            if (p.lat[row][col] == 0)
+            {
+                p.hrz[row][col] = puzzle::BAN;
+                p.hrz[row + 1][col] = puzzle::BAN;
+                p.vrt[row][col] = puzzle::BAN;
+                p.vrt[row][col + 1] = puzzle::BAN;
+            }
+        }
+    }
+}
+
+void DFS(puzzle &p)
+{
+    // For every points find all solutions
     for (size_t row = 0; row <= p.rows; row++)
     {
+        // Optimized
         for (size_t col = 1; col <= p.cols; col += 2)
         {
             go_next(p, row, col, row, col);
             // set point banned
-            p.ban[row][col - 1] = true;
-            p.ban[row][col] = true;
+            p.banned_point[row][col - 1] = true;
+            p.banned_point[row][col] = true;
             if (col + 1 == p.cols)
-                p.ban[row][col + 1] = true;
+                p.banned_point[row][col + 1] = true;
         }
     }
 }
@@ -288,10 +337,10 @@ void go_vrt(puzzle p,
             const int &dst_p_r, const int &dst_p_c)
 {
     // Link overlap
-    if (p.vrt[v_r][v_c])
+    if (p.vrt_has_edge(v_r, v_c))
         return;
     // Draw line
-    p.vrt[v_r][v_c] = 1;
+    p.vrt[v_r][v_c] = puzzle::LINKED;
     // Solved
     if (dst_p_r == start_r && dst_p_c == start_c)
     {
@@ -323,10 +372,10 @@ void go_hrz(puzzle p, const int &start_r, const int &start_c,
             const int &dst_p_r, const int &dst_p_c)
 {
     // Link overlap
-    if (p.hrz[h_r][h_c])
+    if (p.hrz_has_edge(h_r, h_c))
         return;
     // Draw line
-    p.hrz[h_r][h_c] = 1;
+    p.hrz[h_r][h_c] = puzzle::LINKED;
     // Solved
     if (dst_p_r == start_r && dst_p_c == start_c)
     {
@@ -357,28 +406,28 @@ void go_next(puzzle &p,
              const int &start_r, const int &start_c,
              const int &dst_p_r, const int &dst_p_c)
 {
-    if (p.point_has_up(dst_p_r, dst_p_c))
+    if (p.point_can_up(dst_p_r, dst_p_c))
     {
         go_vrt(p,
                start_r, start_c,
                dst_p_r - 1, dst_p_c,
                dst_p_r - 1, dst_p_c);
     }
-    if (p.point_has_down(dst_p_r, dst_p_c))
+    if (p.point_can_down(dst_p_r, dst_p_c))
     {
         go_vrt(p,
                start_r, start_c,
                dst_p_r, dst_p_c,
                dst_p_r + 1, dst_p_c);
     }
-    if (p.point_has_left(dst_p_r, dst_p_c))
+    if (p.point_can_left(dst_p_r, dst_p_c))
     {
         go_hrz(p,
                start_r, start_c,
                dst_p_r, dst_p_c - 1,
                dst_p_r, dst_p_c - 1);
     }
-    if (p.point_has_right(dst_p_r, dst_p_c))
+    if (p.point_can_right(dst_p_r, dst_p_c))
     {
         go_hrz(p,
                start_r, start_c,
